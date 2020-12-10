ed78020fde6e5eb9d4ad80942f0879bfecdc868b
Arlan Dantas <arlanjose@gmail.com>
2020-12-10 17:44:24 -0300

    first example

--- src/simplehttpserver/examples/BasicServer.java
+++ src/simplehttpserver/examples/BasicServer.java
@@ -3,19 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package simplehttpserver.example;
+package simplehttpserver.examples;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import simplehttpserver.*;
+import simplehttpserver.examples.handlers.Handler01;
 
 /**
  *
  * @author arlan
  */
-public class SimpleHttpServer {
+public class BasicServer {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Initial code!");
+        try {
+            SimpleHTTPServer server = new SimpleHTTPServer(8000);
+            server.addRoute("/", new SimpleRedirectHandler("/index.html"));
+            server.addRoute("/", new Handler01());
+            server.start();
+        } catch (IOException ex) {
+            Logger.getLogger(BasicServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(BasicServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
---
+++ src/simplehttpserver/examples/handlers/Handler01.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplehttpserver.examples.handlers;
+
+import java.io.IOException;
+import simplehttpserver.SimpleExchange;
+import simplehttpserver.SimpleHandler;
+
+/**
+ *
+ * @author arlan
+ */
+public class Handler01 extends SimpleHandler {
+
+    @Override
+    public void onGET(SimpleExchange e) throws IOException {
+        e.sendResponse("That is a GET request!");
+    }
+
+    @Override
+    public void onPOST(SimpleExchange e) throws IOException {
+        e.sendResponse("That is a POST request!");
+    }
+
+    @Override
+    public void onPUT(SimpleExchange e) throws IOException {
+        e.sendResponse("That is a PUT request!");
+    }
+
+    @Override
+    public void onDELETE(SimpleExchange e) throws IOException {
+        e.sendResponse("That is a DELETE request!");
+    }
+    
+}
